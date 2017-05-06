0f2b5435f400b86393455d93a4ce54c10b22f658
Simon Olander <simon.olander@r2m.se>
2017-05-06 14:59:12 +0200

    Camera and mesh

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5;
+
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.position - target.position;
+	}
+	
+	void FixedUpdate () {
+		Vector3 targetPosition = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetPosition, smoothing * Time.deltaTime);
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,44 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+	public float speed = 6;
+
+	private Vector3 movement;
+	private Animator anim;
+	private Rigidbody rb;
+	private int floorMask;
+	private float rayLength = 100;
+
+	void Awake() {
+		rb = GetComponent<Rigidbody> ();
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+
+	}
+
+	void FixedUpdate () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		float z = Input.GetAxisRaw ("Vertical");
+
+		movement.Set (x, 0, z);
+		movement = movement.normalized * speed * Time.deltaTime;
+		rb.MovePosition (rb.transform.position + movement);
+
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, rayLength, floorMask)) {
+			Vector3 playerToMouse = hit.point - rb.transform.position;
+			playerToMouse.y = 0;
+
+			Quaternion rotation = Quaternion.LookRotation (playerToMouse);
+			rb.MoveRotation(rotation);
+		}
+
+		bool walking = x != 0 || z != 0;
+		anim.SetBool ("IsWalking", walking);
+	}
+
+	void LateUpdate () {
+
+	}
 }
